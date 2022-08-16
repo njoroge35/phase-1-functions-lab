cc1b8581616e1f2b5f956caa3fa62a40b4f61812
David <david.njoroge@student.moringaschool.com>
2022-08-16 21:11:30 +0300

    functions

--- index.js
+++ index.js
@@ -1,1 +1,47 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(someNumber) {
+
+    if (someNumber >= 42) {
+      return someNumber - 42
+    }
+  
+    else {
+        return 42 - someNumber
+    }
+  
+  }
+  
+  function distanceFromHqInFeet(someNumber) {
+    return distanceFromHqInBlocks(someNumber) * 264
+  }
+  
+  function distanceTravelledInFeet(begin, end) {
+  
+    if (begin >= 1 && end > begin) {
+      return (end - begin) *264
+    }
+    else if (end < begin) {
+      return (begin - end) *264
+    }
+  }
+  
+  function calculatesFarePrice(begin, end) {
+    
+    if (distanceTravelledInFeet(begin, end) < 400) {
+      return 0
+  
+    }
+  
+    else if (distanceTravelledInFeet(begin, end) > 400 && distanceTravelledInFeet(begin, end) <= 2000) {
+      return (distanceTravelledInFeet(begin, end) - 400) * 0.02
+    }
+  
+    else if (distanceTravelledInFeet(begin, end) > 2500) {
+      return 'cannot travel that far'
+    }
+  
+    else if (distanceTravelledInFeet(begin, end) > 2000) {
+      return 25
+    }
+    
+  }
